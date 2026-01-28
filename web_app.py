7192941145d88901a8e9f89b0ad93346c0cc0d15
sazkiaeliyana335-hub <sazkiaeliyana335@gmail.com>
2026-01-28 04:51:33 +0000

    fix web app

--- web_app.py
+++ web_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from core import get_bot_reply
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
